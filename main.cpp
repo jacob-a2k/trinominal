f153758c93d33a3da18422a4409324636ab4746a
jakub <kuba33604@op.pl>
2022-01-17 20:02:19 +0100

    usunąłem biblioteki, sprecyzowałem pytanie, zmieniłem kolejnośc zadań w programie

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
-#include <iostream>
+#include <iostream> 
 #include <cmath>
-#include <stdio.h>
-#include <cstdlib>
-#include <conio.h>
-#include <cstdlib> //zostaw tylko potrzebne biblioteki
+ //zostaw tylko potrzebne biblioteki
 
 using namespace std;
 
@@ -11,33 +8,36 @@ int main()
 {
 
 	float a, b, c; 
-    cout << "Witaj! Podaj 3 liczby!" << endl;
+    cout << "Witaj! Aby obliczyc pierwiastki trojmianu kwadratowego podaj wartosci a, b i c !" << endl;
     //program powinien mówić do czego jest i po co to te trzy liczby 
     cin >> a >> b >> c;
 
+	if(a == 0){
+        cout << "Wspolczynnik a nie moze byc rowny " << a << endl;
+	}
 	float delta = b * b - 4 * a * c;
-
-    //nie porzebnie wykonujesz obliczenia nie widzac ile ani jakie bedą pierwiastki
-    //napierw trzeba swierdzic jakie jest a jaka delta a potem obliczać
-    float jeden = (- b) / (2 * a);// uzywaj angielskich nazw zmiennych
-
-
-    float z = ((-b) - sqrt(delta)) / (2 * a); 
+	
+	if(  delta < 0 ){
+        cout << "Nie ma rozwiazan " << endl;
+	}
+	float one = (- b) / (2 * a);
+	
+	if (delta == 0){
+        cout << "Pierwiastek wynosi: " << one << endl;
+    }
+	float z = ((-b) - sqrt(delta)) / (2 * a); 
 
     float y = ((-b) + sqrt(delta)) / (2 * a);
-
-    if(a == 0){
-        cout << "Wspolczynnik a nie moze byc rowny " << a << endl;
-    }
-    else if(  delta < 0 ){
-        cout << "Nie ma rozwiazan " << endl;
-    }
-    else if (delta == 0){
-        cout << "Pierwiastek wynosi: " << jeden << endl;
-    }
-    else if (delta > 0){
+	
+	if (delta > 0){
         cout << " Pierwszy pierwiastek wynosi: "<< z << endl;
         cout << " Drugi pierwiastek wynosi : "<< y << endl;
     }
-}
+}	
+    //nie porzebnie wykonujesz obliczenia nie widzac ile ani jakie bedą pierwiastki
+    //napierw trzeba swierdzic jakie jest a jaka delta a potem obliczać
+ // uzywaj angielskich nazw zmiennych
+
+
+  
 
