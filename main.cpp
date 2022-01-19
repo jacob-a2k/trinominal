46f94ba88657bdc5c7c260ad8018fda10552c56d
jakub <kuba33604@op.pl>
2022-01-19 18:40:14 +0100

    poprawa obliczen, program wykonuje tylko niezbedne obliczenia

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main()
 {
 
 	float a, b, c; 
+	cout << endl;
     cout << "Witaj! Aby obliczyc pierwiastki trojmianu kwadratowego podaj wartosci a, b i c !" << endl;
     cin >> a >> b >> c;
 
@@ -19,25 +20,20 @@ int main()
   // jeżeli a = 0 to nie ma sensu wykowywać innych obliczeń itd
 	float delta = b * b - 4 * a * c;
 	
-	if(  delta < 0 ){
+	float xOne = ((-b) - sqrt(delta)) / (2 * a); 
+
+    float xTwo = ((-b) + sqrt(delta)) / (2 * a);
+		
+	if ( delta < 0 ){
         cout << "Nie ma rozwiazan " << endl;
 	}
-	float one = (- b) / (2 * a);
-	 
-	if (delta == one){
-        cout << "Pierwiastek wynosi: " << one << endl;
-    }
-	float z = ((-b) - sqrt(delta)) / (2 * a); 
-
-    float y = ((-b) + sqrt(delta)) / (2 * a);
-	
-	if (delta > 0){
-        cout << " Pierwszy pierwiastek wynosi: "<< z << endl;
-        cout << " Drugi pierwiastek wynosi : "<< y << endl;
-    }
+	else if (delta == 0){
+		float deltaZero = (- b) / (2 * a);
+        cout << "Pierwiastek wynosi: " << deltaZero << endl;
+	}
+	else if (delta > 0){
+        cout << " Pierwszy pierwiastek wynosi: "<< xOne << endl;
+        cout << " Drugi pierwiastek wynosi : "<< xTwo << endl;
+	}
+	return (0);
 }	
-	
-
-
-  
-
