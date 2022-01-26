e0f6ce874745130f8b03486fcae6b0303619732c
jakub <kuba33604@op.pl>
2022-01-26 20:28:17 +0100

    wprowadzenie mozliwosc ponownego obliczenia pierwiastkow lub zamkniecia programu

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
-#include <iostream> 
+#include <iostream>
 #include <cmath>
+#include <cstdlib>
+#include <cstdio>
+#include <conio.h>
 
 using namespace std;
 
 int main()
 {
-	float a, b, c; 
+for(;;)
+{
+	float a, b, c;
     cout << "\nWitaj! Aby obliczyc pierwiastki trojmianu kwadratowego podaj wartosci a, b i c !" << endl;
     cin >> a >> b >> c;
 
@@ -14,9 +19,9 @@ int main()
 		cout << "\nPodaj inna wartosc a!" << endl;
 		cin >> a;
 	}
-	
+
 	float delta = b * b - 4 * a * c;
-	
+
 	if (delta < 0) {
         cout << "Nie ma rozwiazan " << endl;
 	}
@@ -25,11 +30,30 @@ int main()
         cout << "Pierwiastek wynosi: " << x1 << endl;
 	}
 	else {
-		float x1 = ((-b) - sqrt(delta)) / (2 * a); 
+		float x1 = ((-b) - sqrt(delta)) / (2 * a);
         cout << " Pierwszy pierwiastek wynosi: "<< x1 << endl;
-		
+
 		float x2 = ((-b) + sqrt(delta)) / (2 * a);
         cout << " Drugi pierwiastek wynosi : "<< x2 << endl;
 	}
-	return 0;
-}	
+    cout << endl;
+	cout << "Czy chcesz ponownie obliczyc pierwiastki trojmianu kwadratowego?";
+	cout << "\nWcisnij t aby ponownie dokonac obliczen!";
+	cout << "\nWcisnij n aby zakonczyc program!" << endl;
+
+    char wybor;
+	wybor = getch();
+
+	switch(wybor){
+    case 't':
+        system("cls");
+    break;
+    case 'n':
+        exit(0);
+    break;
+
+    default: cout << "Nie ma takiej opcji" << endl;
+        exit(0);
+	}
+}
+}
